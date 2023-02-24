a2a92d3f79a8f9ad04d98fa76b2c7a57b2ac858d
Prajwal Padal <prajwal_padal@persistent.com>
2023-02-24 15:59:37 +0530

    themeData added

---
+++ lib/constant.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const scafBgColor = Color(0xFFF2F3F8);
+const bgShadow = Color(0xFFf4f3f3);
+const kColorPrimary = Color(0xFF3BC279);
+
+const MaterialColor kPrimarySwatchColor = MaterialColor(
+    0xFF3BC279,
+    <int, Color>{
+      50: Color(0xFF3BC279),
+      100: Color(0xFF3BC279),
+      200: Color(0xFF3BC279),
+      300: Color(0xFF3BC279),
+      400: Color(0xFF3BC279),
+      500: Color(0xFF3BC279),
+      600: Color(0xFF3BC279),
+      700: Color(0xFF3BC279),
+      800: Color(0xFF3BC279),
+      900: Color(0xFF3BC279),
+    });
