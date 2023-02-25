84cded8ea770c388972c6a7dc253d099fbea18c5
vishwajeet-waychal <vishwajeet.w562@gmail.com>
2023-02-25 23:01:00 +0530

    home page updated

--- lib/views/past_activities.dart
+++ lib/views/past_activities.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:psl_foundation/views/registered_participants_page.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:psl_foundation/views/widgets/appbar.dart';
+
+import '../constant.dart';
 class PastActivities extends StatelessWidget {
   const PastActivities({Key? key}) : super(key: key);
 
