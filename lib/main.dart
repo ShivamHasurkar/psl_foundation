e39cb546630de95b751f974251f4512b64484245
vishwajeet-waychal <vishwajeet.w562@gmail.com>
2023-02-26 03:14:18 +0530

    home screen routing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'constant.dart';
 import 'views/splash_screen.dart';
@@ -8,6 +9,7 @@ import 'views/splash_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
